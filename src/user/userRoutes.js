dd5e922a2814778b616a5ee3959c484374c461f2
[Tom Fox] <[tommo120@gmail.com]>
2021-12-22 16:50:58 +0000

    Properly enabled cors?

--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,13 +1,19 @@
+const cors = require('cors');
 const { Router } = require("express");
 const { hashPassword, comparePasswords } = require("../middleware/index.js");
 const { addUser, listUsers, findUser, updateUser, updatePassword, deleteUser } = require("./userController.js");
 const userRouter = Router();
 
-userRouter.post("/user", hashPassword, addUser);
-userRouter.get("/user", listUsers);
-userRouter.post("/login", findUser, comparePasswords);
-userRouter.put("/updatePassword", findUser, comparePasswords, hashPassword, updatePassword);
-userRouter.put("/user", updateUser)
-userRouter.delete("/user", deleteUser);
+var corsOptions = {
+    origin: 'https://serene-mccarthy-223972.netlify.app/',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+  }
+
+userRouter.post("/user", cors(), hashPassword, addUser);
+userRouter.get("/user", cors(), listUsers);
+userRouter.post("/login", cors(), findUser, comparePasswords);
+userRouter.put("/updatePassword", cors(), findUser, comparePasswords, hashPassword, updatePassword);
+userRouter.put("/user", cors(), updateUser)
+userRouter.delete("/user", cors(), deleteUser);
 
 module.exports = userRouter;
