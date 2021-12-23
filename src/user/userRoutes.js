359bdccc9fcd19e86a004a6d4be975e4721e7576
[Tom Fox] <[tommo120@gmail.com]>
2021-12-23 13:34:37 +0000

    Removed CORS syntax from user routes

--- src/user/userRoutes.js
+++ src/user/userRoutes.js
@@ -1,19 +1,13 @@
-const cors = require('cors');
 const { Router } = require("express");
 const { hashPassword, comparePasswords } = require("../middleware/index.js");
 const { addUser, listUsers, findUser, updateUser, updatePassword, deleteUser } = require("./userController.js");
 const userRouter = Router();
 
-var corsOptions = {
-    origin: 'https://serene-mccarthy-223972.netlify.app/',
-    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-  }
-
-userRouter.post("/user", cors(corsOptions), hashPassword, addUser);
-userRouter.get("/user", cors(corsOptions), listUsers);
-userRouter.post("/login", cors(corsOptions), findUser, comparePasswords);
-userRouter.put("/updatePassword", cors(corsOptions), findUser, comparePasswords, hashPassword, updatePassword);
-userRouter.put("/user", cors(corsOptions), updateUser)
-userRouter.delete("/user", cors(corsOptions), deleteUser);
+userRouter.post("/user", hashPassword, addUser);
+userRouter.get("/user", listUsers);
+userRouter.post("/login", findUser, comparePasswords);
+userRouter.put("/updatePassword", findUser, comparePasswords, hashPassword, updatePassword);
+userRouter.put("/user", updateUser)
+userRouter.delete("/user", deleteUser);
 
 module.exports = userRouter;
